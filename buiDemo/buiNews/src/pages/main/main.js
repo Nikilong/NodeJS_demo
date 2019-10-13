8ac88f629fba718f313cc2b5134202ffc891dd53
Niki <Niki@NikideMacBook.local>
2019-10-13 16:57:16 +0800

    增加搜索的热点功能，以及优化部分UI

--- buiDemo/buiNews/src/pages/main/main.js
+++ buiDemo/buiNews/src/pages/main/main.js
@@ -19,7 +19,7 @@ loader.define(function(require, exports, module) {
         });
 
         // 先初始化tab
-        var uiTab = bui.tab({
+        pageview.uiTab = bui.tab({
             id:"#uiTabNavbar",
             menu:"#nav",
             // 1: 声明是动态加载的tab
@@ -28,7 +28,7 @@ loader.define(function(require, exports, module) {
         });
 
         // 监听加载后的事件
-        uiTab.on("to", function(index) {
+        pageview.uiTab.on("to", function(index) {
             console.log(index, "channel----")
             // 有滚动条时在居中显示
             var itemwidth = $("#nav li").eq(index).prev().width();
@@ -39,22 +39,79 @@ loader.define(function(require, exports, module) {
             G_currendChannelIndex = index;
         }).to(0);
 
+
         // 2.弹出的频道列表初始化
+        _this.initChannel();
+        
+        // 3.搜索页面
+        _this.initSearchPage();
+        
+    }
+    
+    pageview.event = function(){
+        let _this = pageview;
+
+        router.$("#btnSearchDialog").on("click", function(argument) {
+            _this.searchDialog.open();
+        })
+        router.$("#btnBack").on("click", function(argument) {
+            _this.searchDialog.close();
+        })
+        // router.$("#btnSearch").on("click", function(argument) {
+        //     uiSearchbar.search();
+        // })
+
+        // 搜索
+        router.$("#search").on("click",function(){
+            _this.searchDialog.open();
+        });
+        // 弹出地铁图
+        router.$("#metro-btn").on("click",function(){
+            bui.load({url:"pages/metro-detail/metro-detail.html",param:{"cityName":G_localcity} });
+        });
+        // 弹出按钮的事件绑定
+        router.$("#moreChannelBtn").on("click",function(){
+            _this.uiDialogNav.open();
+            router.$("#channelDialog").find("input").eq(G_currendChannelIndex).attr("checked",true);
+        });
+    
+        // (弹出界面)选择了频道的事件绑定
+        router.$("#channelDialog").on("click",function(e){
+            e.stopPropagation();
+            let ele = $(e.target);
+            if(ele[0].tagName.toLowerCase() === "input"){  
+                // 收起弹框
+                _this.uiDialogNav.close();
+                // 切换tab
+                _this.uiTab.to(ele.attr("data-index"));
+            }
+        });
+        
+        // 回滚到顶部
+        router.$("#scrollTotopBtn").on("click",function(e){
+            let targetEle = $("#uiScroll"+(G_currendChannelIndex + 1))[0];
+            common.scrollAnimation(targetEle,targetEle.scrollTop,0);
+        })
+    
+    };
+
+    // 2.弹出的频道列表初始化
+    pageview.initChannel = function(){
         // 添加栏目
         let html = "";
         G_channelConfig.forEach((ele,index) => {
             html += `
-                <li class="span3">
+                <li class="span3" style="margin-bottom:0.1rem;">
                     <div class="bui-check">
-                        <input id="uid${index}" name="channel" type="radio" data-index=${index}><label for="uid${index}">${ele.name}</label>
+                        <input id="uid${index}" name="channel" type="radio" data-index=${index}><label class="ring" for="uid${index}">${ele.name}</label>
                     </div>
                 </li>
             `;
         });
-        $("#channelDialog").html(html);
+        router.$("#channelDialog").html(html);
 
         // 3.栏目弹出菜单, 因为要遮住底部的导航, 所以弹出层需要跟底部导航在一块初始化
-        var uiDialogNav = bui.dialog({
+        pageview.uiDialogNav = bui.dialog({
             id: "#uiDialog",
             position:"right",
             fullscreen: true,
@@ -63,11 +120,11 @@ loader.define(function(require, exports, module) {
         });
 
         // 显示当前城市名称
-        $(".location_city").text(G_localcity).show();
+        router.$(".location_city").text(G_localcity).show();
+    }
 
-
-
-        // 4.搜索页面
+    // 3.搜索页面
+    pageview.initSearchPage = function(){
         var searchDialog = bui.dialog({
             id: "#searchUIDialog",
             fullscreen: true,
@@ -81,12 +138,19 @@ loader.define(function(require, exports, module) {
             id: "#searchbar",
             onInput: function(ui, keyword) {
                 //实时搜索
-                console.log(keyword);
-                this.search(keyword);
+                if(keyword && keyword.length > 0){
+                    this.search(keyword);
+                    router.$("#hotNews-container").hide();
+                }else{
+                    router.$("#scrollSearch").html("")
+                    router.$("#hotNews-container").show();
+                }
             },
             onRemove: function(ui, keyword) {
                 //删除关键词需要做什么其它处理
-                console.log(keyword);
+                console.log("onRemove",keyword);
+                router.$("#scrollSearch").html("")
+                router.$("#hotNews-container").show();
             },
             callback: function(ui, keyword) {
                 let params = {
@@ -94,24 +158,27 @@ loader.define(function(require, exports, module) {
                     PARAMS: {keyword:keyword},
                     SERVICE:  "NewsService.searchNews"
                 }
+
                 common.newsServers(params,function(data){
-                    console.log(data)
                     try{
                         data = JSON.parse(data)
                         if(data.status === "success"){
-                            let list = data.RESULT.data.sug
+                            let list = data.RESULT.data.sug;
+                            // 关键词高亮
+                            let keyw = data.RESULT.data.query.replace("$","");
                             if(list && list.length > 0){
                                 let html = "";
                                 list.forEach((item,i)=>{
                                     html += `
                                     <li class="bui-btn bui-box" href="pages/detail/detail.html?keyword=${encodeURI(item.word)}">
-                                        <p class="item-text">${item.word}</p>
+                                        <p class="item-text"><span class="keyword">${keyw}</span>${item.word.replace(keyw,"")}</p>
                                     </li>`
                                 })
                                 
-                                $("#scrollSearch").html(html)
+                                router.$("#scrollSearch").html(html)
                             }else{
-                                bui.alert("没有查询到数据");
+                                router.$("#scrollSearch").html("")
+                                common.toast("fail","没有查询到数据");
                             }
                         }
 
@@ -121,54 +188,30 @@ loader.define(function(require, exports, module) {
 
         });
 
+        // 热点新闻加载
+        let paramsHot = {
+            SERVICE:  "NewsService.getHotNews"
+        }
+        common.newsServers(paramsHot,function(data){
+            
+            try{
+                data = JSON.parse(data);
+                let list = JSON.parse(data.RESULT);
+                let hotNewsHtml = "";
+                list.forEach((item,i)=>{
+                    hotNewsHtml += `
+                        <li class="span6">
+                            <div class="bui-btn" href="pages/detail/detail.html?keyword=${item.title}">${item.title}</div>
+                        </li>
+                    `;
+                })
 
-        $("#btnSearchDialog").on("click", function(argument) {
-            pageview.searchDialog.open();
-        })
-        $("#btnBack").on("click", function(argument) {
-            pageview.searchDialog.close();
-        })
-        $("#btnSearch").on("click", function(argument) {
-            uiSearchbar.search();
-        })
+                router.$("#hotNews-container").find(".bui-fluid").html(hotNewsHtml);
 
-    }
-    
-    pageview.event = function(){
-        let _this = pageview;
-        
-        // 搜索
-        $("#search").on("click",function(){
-            _this.searchDialog.open();
+            }catch(e){console.log(e)}
         });
-        // 弹出地铁图
-        $("#metro-btn").on("click",function(){
-            bui.load({url:"pages/metro-detail/metro-detail.html",param:{"cityName":G_localcity} });
-        });
-        // 弹出按钮的事件绑定
-        $("#moreChannelBtn").on("click",function(){
-            uiDialogNav.open();
-            $("#channelDialog").find("input").eq(G_currendChannelIndex).attr("checked",true);
-        });
-    
-        // (弹出界面)选择了频道的事件绑定
-        $("#channelDialog").on("click",function(e){
-            e.stopPropagation();
-            let ele = $(e.target);
-            if(ele[0].tagName.toLowerCase() === "input"){  
-                // 收起弹框
-                uiDialogNav.close();
-                // 切换tab
-                uiTab.to(ele.attr("data-index"));
-            }
-        });
-        
-        // 回滚到顶部
-        $("#scrollTotopBtn").on("click",function(e){
-            let targetEle = $("#uiScroll"+(G_currendChannelIndex + 1))[0];
-            common.scrollAnimation(targetEle,targetEle.scrollTop,0);
-        })
-    
+
+
     };
 
     // 初始化
